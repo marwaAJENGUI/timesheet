dbfade547cfb09cbad7f8ec38e9f4edeab339137
Amine Youssef <amine.youssef@esprit.tn>
2020-10-29 20:53:09 +0100

    Tests JUnit

--- src/test/java/tn/esprit/spring/controller/ControllerEmployeImplTest.java
+++ src/test/java/tn/esprit/spring/controller/ControllerEmployeImplTest.java
@@ -1,5 +1,6 @@
 package tn.esprit.spring.controller;
 
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -33,36 +34,36 @@ public class ControllerEmployeImplTest {
 
 	private static final Logger logger = Logger.getLogger(ControllerEmployeImplTest.class);
 
-	
 	private static String login;
 	private static String password;
 	private Role role;
 	private Boolean loggedIn;
 
+	int employeIdToBeUpdated=1;
 	private Employe authenticatedUser = null;
 	private String prenom;
 	private String nom;
 	private String email;
 	private boolean actif;
-	
+
 	static String requestJson;
 	static HttpHeaders headers;
 	static String strDateDebut;
 	static String strDateFin;
-	
+
 	@Autowired
 	IEmployeService employeService;
-	
+
 	@BeforeClass
-	 public static void setUp() {
-		login="test";
-		password="test";
+	public static void setUp() {
+		login = "Khaled.kallel@ssiiconsulting.tn";
+		password = "aaa";
 		headers = new HttpHeaders();
-	    headers.setContentType(MediaType.APPLICATION_JSON);
-	    strDateDebut="28-10-2020";
-	    strDateFin="03-11-2020";
+		headers.setContentType(MediaType.APPLICATION_JSON);
+		strDateDebut = "28-10-2020";
+		strDateFin = "03-11-2020";
 	}
-	
+
 	@Test
 	public void doLoginTest() {
 		logger.debug("entering doLoginTest()...");
@@ -71,7 +72,7 @@ public class ControllerEmployeImplTest {
 			logger.debug("getting user info...");
 			authenticatedUser = employeService.authenticate(login, password);
 			logger.debug("Authenticating user: " + login + "/" + password);
-			if (authenticatedUser != null && authenticatedUser.getRole() == Role.ADMINISTRATEUR) {
+			if (authenticatedUser != null || authenticatedUser.getRole() == Role.ADMINISTRATEUR) {
 				logger.debug("used Authenticated!");
 				navigateTo = "/pages/admin/welcome.xhtml?faces-redirect=true";
 				loggedIn = true;
@@ -79,19 +80,16 @@ public class ControllerEmployeImplTest {
 
 			else {
 				logger.debug("Authentication failed of user " + login + "/" + password + " failed");
-				FacesMessage facesMessage = new FacesMessage(
-						"Login Failed: Please check your username/password and try again.");
-				FacesContext.getCurrentInstance().addMessage("form:btn", facesMessage);
 				logger.warn("exiting doLogin() with failed authentication...");
 			}
-			
-			assertEquals(navigateTo,"/pages/admin/welcome.xhtml?faces-redirect=true");
+
+			assertEquals(navigateTo, "/pages/admin/welcome.xhtml?faces-redirect=true");
 			assertTrue(loggedIn);
 		} catch (Exception e) {
-			logger.error("exiting doLogin() with "+ e + "error");
+			logger.error("exiting doLogin() with " + e + " error");
 		}
 	}
-	
+
 	@Test
 	public void doLogoutTest() {
 		logger.debug("entering doLogoutTest()...");
@@ -102,10 +100,50 @@ public class ControllerEmployeImplTest {
 			String result = "/login.xhtml?faces-redirect=true";
 			assertEquals(result, "/login.xhtml?faces-redirect=true");
 		} catch (Exception e) {
-			logger.error("exiting doLogoutTest() with "+ e + "error");
+			logger.error("exiting doLogoutTest() with " + e + " error");
+		}
+	}
+
+	@Test
+	public void addEmployeTest() {
+		logger.debug("entering addEmploye()...");
+		try {
+			Employe employe = new Employe(nom, prenom, login, password, actif, role);
+			logger.debug("adding employe " + employe);
+			String addedEmployePrenom = employeService.getEmployePrenomById(employeService.addOrUpdateEmploye(employe));
+			logger.debug("employe added, exiting addEmploye()...");
+			assertEquals(addedEmployePrenom, employe.getPrenom());
+		} catch (Exception e) {
+			logger.error("exiting addEmployeTest() with " + e + " error");
 		}
 	}
 	
+	@Test
+	public void removeEmployeTest() {
+		logger.debug("entering removeEmploye()...");
+		int employeId=0;
+		try {
+			logger.debug("deleting employe " + employeService.getEmployePrenomById(employeId));
+			employeService.deleteEmployeById(employeId);
+			logger.debug("employe deleted, exiting removeEmploye()...");
+			assertNull(employeService.getEmployePrenomById(employeId));
+		} catch (Exception e) {
+			logger.error("exiting removeEmployeTest() with " + e + " error");
+		}
+	}
 	
+	@Test
+	public void updateEmploye() {
+		logger.debug("entering updateEmploye()...");
+		try {
+			Employe employe = new Employe(employeIdToBeUpdated, nom, prenom, email, password, actif, role);
+
+			logger.debug("updating employe to " + employe);
+			employeService.addOrUpdateEmploye(employe);
+			logger.debug("Employe updated, exiting updateEmploye()");
+		} catch (Exception e) {
+			logger.error("exiting updateEmployeTest() with " + e + " error");
+		}
+	}
 
 }
