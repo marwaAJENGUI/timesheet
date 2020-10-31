c95255661c93a9305984844cbecaac7444c1c726
AymenBrahim <brahimaymen.brahim@esprit.tn>
2020-10-31 23:52:58 +0100

    Logging 1.0
    RestControlEntreprise

--- src/main/java/tn/esprit/spring/controller/RestControlEntreprise.java
+++ src/main/java/tn/esprit/spring/controller/RestControlEntreprise.java
@@ -2,6 +2,7 @@ package tn.esprit.spring.controller;
 
 import java.util.List;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,67 +22,83 @@ import tn.esprit.spring.services.ITimesheetService;
 @RestController
 public class RestControlEntreprise {
 
-	
 	@Autowired
 	IEmployeService iemployeservice;
 	@Autowired
 	IEntrepriseService ientrepriseservice;
 	@Autowired
 	ITimesheetService itimesheetservice;
-	
-	// Ajouter Entreprise : http://localhost:8081/SpringMVC/servlet/ajouterEntreprise
-	//{"id":1,"name":"SSII Consulting","raisonSocial":"Cite El Ghazela"}
+	private static final Logger logger = Logger.getLogger(RestControlEntreprise.class);
 
+	// Ajouter Entreprise :
+	// http://localhost:8081/SpringMVC/servlet/ajouterEntreprise
+	// {"id":1,"name":"SSII Consulting","raisonSocial":"Cite El Ghazela"}
 	@PostMapping("/ajouterEntreprise")
 	@ResponseBody
 	public int ajouterEntreprise(@RequestBody Entreprise ssiiConsulting) {
+		logger.info("In Controller:ajouterEntreprise() : ");
 		ientrepriseservice.ajouterEntreprise(ssiiConsulting);
+		logger.info("Out Of ajouterEntreprise() : ");
 		return ssiiConsulting.getId();
 	}
-	
+
 	// http://localhost:8081/SpringMVC/servlet/affecterDepartementAEntreprise/1/1
-    @PutMapping(value = "/affecterDepartementAEntreprise/{iddept}/{identreprise}") 
-	public void affecterDepartementAEntreprise(@PathVariable("iddept")int depId, @PathVariable("identreprise")int entrepriseId) {
+	@PutMapping(value = "/affecterDepartementAEntreprise/{iddept}/{identreprise}")
+	public void affecterDepartementAEntreprise(@PathVariable("iddept") int depId,
+			@PathVariable("identreprise") int entrepriseId) {
+		logger.info("In Controller:affecterDepartementAEntreprise() : ");
 		ientrepriseservice.affecterDepartementAEntreprise(depId, entrepriseId);
+		logger.info("Out Of Controller:affecterDepartementAEntreprise()");
 	}
-    
-    // http://localhost:8081/SpringMVC/servlet/deleteEntrepriseById/1
-    @DeleteMapping("/deleteEntrepriseById/{identreprise}") 
-	@ResponseBody 
-	public void deleteEntrepriseById(@PathVariable("identreprise")int entrepriseId)
-	{
+
+	// http://localhost:8081/SpringMVC/servlet/deleteEntrepriseById/1
+	@DeleteMapping("/deleteEntrepriseById/{identreprise}")
+	@ResponseBody
+	public void deleteEntrepriseById(@PathVariable("identreprise") int entrepriseId) {
+		logger.info("In Controller:deleteEntrepriseById() : ");
 		ientrepriseservice.deleteEntrepriseById(entrepriseId);
+		logger.info("Out of Controller:deleteEntrepriseById() : ");
 	}
-    
-    // http://localhost:8081/SpringMVC/servlet/getEntrepriseById/1
-    @GetMapping(value = "getEntrepriseById/{identreprise}")
-    @ResponseBody
+
+	// http://localhost:8081/SpringMVC/servlet/getEntrepriseById/1
+	@GetMapping(value = "getEntrepriseById/{identreprise}")
+	@ResponseBody
 	public Entreprise getEntrepriseById(@PathVariable("identreprise") int entrepriseId) {
-
-		return ientrepriseservice.getEntrepriseById(entrepriseId);
+		logger.info("In Controller:affecterDepartementAEntreprise() : ");
+		Entreprise e = ientrepriseservice.getEntrepriseById(entrepriseId);
+		logger.info("In Controllere:affecterDepartementAEntreprise() : ");
+		return e;
 	}
-    
-    // http://localhost:8081/SpringMVC/servlet/ajouterDepartement
- 	//{"id":1,"name":"Telecom"}
 
- 	@PostMapping("/ajouterDepartement")
- 	@ResponseBody
+	// http://localhost:8081/SpringMVC/servlet/ajouterDepartement
+	// {"id":1,"name":"Telecom"}
+
+	@PostMapping("/ajouterDepartement")
+	@ResponseBody
 	public int ajouterDepartement(@RequestBody Departement dep) {
-		return ientrepriseservice.ajouterDepartement(dep);
-	}
-	
- 	 // http://localhost:8081/SpringMVC/servlet/getAllDepartementsNamesByEntreprise/1
-    @GetMapping(value = "getAllDepartementsNamesByEntreprise/{identreprise}")
-    @ResponseBody
-	public List<String> getAllDepartementsNamesByEntreprise(@PathVariable("identreprise") int entrepriseId) {
-		return ientrepriseservice.getAllDepartementsNamesByEntreprise(entrepriseId);
+		logger.info("In Controller:affecterDepartementAEntreprise() : ");
+		int id = ientrepriseservice.ajouterDepartement(dep);
+		logger.info("In Controller:affecterDepartementAEntreprise() : ");
+		return id;
 	}
 
-    // URL : http://localhost:8081/SpringMVC/servlet/deleteDepartementById/3
-    @DeleteMapping("/deleteDepartementById/{iddept}") 
-	@ResponseBody 
+	// http://localhost:8081/SpringMVC/servlet/getAllDepartementsNamesByEntreprise/1
+	@GetMapping(value = "getAllDepartementsNamesByEntreprise/{identreprise}")
+	@ResponseBody
+	public List<String> getAllDepartementsNamesByEntreprise(@PathVariable("identreprise") int entrepriseId) {
+		logger.info("In Controller:affecterDepartementAEntreprise() : ");
+		List<String> entreprises = ientrepriseservice.getAllDepartementsNamesByEntreprise(entrepriseId);
+		logger.info("In Controller:affecterDepartementAEntreprise() : ");
+		return entreprises;
+	}
+
+	// URL : http://localhost:8081/SpringMVC/servlet/deleteDepartementById/3
+	@DeleteMapping("/deleteDepartementById/{iddept}")
+	@ResponseBody
 	public void deleteDepartementById(@PathVariable("iddept") int depId) {
+		logger.info("In Controller:affecterDepartementAEntreprise() : ");
 		ientrepriseservice.deleteDepartementById(depId);
+		logger.info("In Controller:affecterDepartementAEntreprise() : ");
 
 	}
 }
