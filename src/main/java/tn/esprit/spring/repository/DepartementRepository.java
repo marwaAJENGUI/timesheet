c33192725744dbdf385df0a956cae2fe5fa5e9a6
AymenBrahim <brahimaymen.brahim@esprit.tn>
2020-11-04 22:21:03 +0100

    Junit

--- src/main/java/tn/esprit/spring/repository/DepartementRepository.java
+++ src/main/java/tn/esprit/spring/repository/DepartementRepository.java
@@ -4,8 +4,9 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import tn.esprit.spring.entities.Departement;
+import tn.esprit.spring.entities.Entreprise;
 
 @Repository
 public interface DepartementRepository extends CrudRepository<Departement, Integer>{
-
+	Departement findTopByOrderByIdDesc();
 }
--- src/test/java/tn/esprit/spring/controller/RestControlEntrepriseTest.java
+++ src/test/java/tn/esprit/spring/controller/RestControlEntrepriseTest.java
@@ -1,41 +1,75 @@
 package tn.esprit.spring.controller;
+import static org.junit.Assert.assertEquals;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import java.net.URI;
+import java.net.URISyntaxException;
 
-import java.util.List;
-
-import org.apache.http.HttpResponse;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
+import org.apache.log4j.Logger;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import org.springframework.web.client.RestTemplate;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import tn.esprit.spring.entities.Departement;
+import tn.esprit.spring.entities.Entreprise;
+import tn.esprit.spring.repository.DepartementRepository;
+import tn.esprit.spring.repository.EntrepriseRepository;
+import tn.esprit.spring.services.EntrepriseServiceImpl;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import org.springframework.http.ResponseEntity;
-
 @RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class RestControlEntrepriseTest {
 	@Autowired
-	private  RestControlEntreprise ec;
+	private EntrepriseServiceImpl es;
+	@Autowired
+	private EntrepriseRepository er;
+	@Autowired
+	private DepartementRepository dr;
+	@LocalServerPort
+	private int port;
+	@Autowired
+	private TestRestTemplate restTemplate;
+	private static final Logger logger = Logger.getLogger(RestControlEntrepriseTest.class);
+	private static Entreprise entreprise=new Entreprise();
+	private static Departement departement=new Departement();
 	
-	 @Test
-	 public void getAllEmployeByMissionTest() throws JsonProcessingException {
-		 CloseableHttpClient client = HttpClients.createDefault();
-		    HttpPost httpPost = new HttpPost("http://www.example.com");
-		 
-		    String json = "{"id":1,"name":"John"}";
-		    StringEntity entity = new StringEntity(json);
-		    httpPost.setEntity(entity);
-		    httpPost.setHeader("Accept", "application/json");
-		    httpPost.setHeader("Content-type", "application/json");
-		 
-		    CloseableHttpResponse response = client.execute(httpPost);
-		    assertThat(response.getStatusLine().getStatusCode(), equalTo(200));
-		    client.close();
+	@BeforeClass
+	 public static void setUp() {
+		
+		
 	}
-
+	@Test
+	public void ajouterEntrepriseTest()
+	{
+		logger.info("In Test:ajouterEntreprise() : ");
+		entreprise.setId(er.findTopByOrderByIdDesc().getId()+1);
+		entreprise.setName("hello");
+		entreprise.setRaisonSocial("bla");
+		assertEquals("L'entreprise n'etait pas bien ajouté",this.restTemplate.postForObject("http://localhost:" + port + "/SpringMVC/servlet/ajouterEntreprise" ,entreprise,
+				String.class),String.valueOf(entreprise.getId()));
+		logger.info("Out Of Test:ajouterEntreprise() : ");
+		System.out.println();
+		//System.out.println(ec.ajouterEntreprise(entreprise));
+	}
+	@Test
+	public void ajouterDepartement() {
+		logger.info("In Test:ajouterDepartement() : ");
+		departement.setEntreprise(entreprise);
+		departement.setId(dr.findTopByOrderByIdDesc().getId()+1);
+		departement.setName("départementX");
+		
+		logger.info("Out of Test:ajouterDepartement() : ");
+	}
 }
